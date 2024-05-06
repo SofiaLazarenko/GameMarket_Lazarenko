d02dff65914b7c2b61a36ac0dd292c16d560ddae
SofiaLazarenko <159002554+SofiaLazarenko@users.noreply.github.com>
2024-05-06 11:15:04 +0300

    j

---
+++ n/market.js
@@ -0,0 +1,37 @@
+console.log('Перевірка')
+
+let itemDiv = document.getElementById("items");
+
+console.log(itemDiv)
+
+if(itemDiv){
+
+     console.log(itemDiv)
+    // console.log("Поле classList:",itemDiv.classList)
+    // console.log("Поле id:",itemDiv.id)
+    // console.log("Поле clientWidth:",itemDiv.clientWidth)
+    // console.log("Поле innerHTML:",itemDiv.innerHTML)
+
+    // itemDiv.innerText="Програмно доданий текст"
+    // itemDiv.innerText+="Програмно доданий текст№2"
+
+    itemDiv.innerHTML+='<div class= "item"></div>'
+   
+    itemDiv.innerHTML+='<div class= "item"></div>'
+   
+    itemDiv.innerHTML+='<div class= "item"></div>'
+   
+    itemDiv.innerHTML+='<div class= "item"></div>'
+   
+    for(let i=0; i<100; i++){
+        itemDiv.innerHTML+='<div class="item"></div>'
+    }
+
+
+
+}else{
+    console.log("NOt found")
+}
+
+
+
