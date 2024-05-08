2363a04504802f29a622d0a9b2e195f6a4298270
SofiaLazarenko <159002554+SofiaLazarenko@users.noreply.github.com>
2024-05-08 11:19:10 +0300

    g

--- n/market.js
+++ n/market.js
@@ -6,26 +6,26 @@ console.log(itemDiv)
 
 if(itemDiv){
 
-     console.log(itemDiv)
-    // console.log("Поле classList:",itemDiv.classList)
-    // console.log("Поле id:",itemDiv.id)
-    // console.log("Поле clientWidth:",itemDiv.clientWidth)
-    // console.log("Поле innerHTML:",itemDiv.innerHTML)
+    //  console.log(itemDiv)
+    // // console.log("Поле classList:",itemDiv.classList)
+    // // console.log("Поле id:",itemDiv.id)
+    // // console.log("Поле clientWidth:",itemDiv.clientWidth)
+    // // console.log("Поле innerHTML:",itemDiv.innerHTML)
 
-    // itemDiv.innerText="Програмно доданий текст"
-    // itemDiv.innerText+="Програмно доданий текст№2"
+    // // itemDiv.innerText="Програмно доданий текст"
+    // // itemDiv.innerText+="Програмно доданий текст№2"
 
-    itemDiv.innerHTML+='<div class= "item"></div>'
+    // itemDiv.innerHTML+='<div class= "item"></div>'
    
-    itemDiv.innerHTML+='<div class= "item"></div>'
+    // itemDiv.innerHTML+='<div class= "item"></div>'
    
-    itemDiv.innerHTML+='<div class= "item"></div>'
+    // itemDiv.innerHTML+='<div class= "item"></div>'
    
-    itemDiv.innerHTML+='<div class= "item"></div>'
+    // itemDiv.innerHTML+='<div class= "item"></div>'
    
-    for(let i=0; i<100; i++){
-        itemDiv.innerHTML+='<div class="item"></div>'
-    }
+    // for(let i=0; i<100; i++){
+    //     itemDiv.innerHTML+='<div class="item"></div>'
+    // }
 
 
 
@@ -35,3 +35,12 @@ if(itemDiv){
 
 
 
+let itemsArray=['r',
+'r',
+'r',
+'r']
+
+itemsArray=itemsArray.sort()
+for(let i=0; i<itemsArray.length[1];i++){
+    console.log(i+ 'й елеиент',itemsArray[i])
+}
