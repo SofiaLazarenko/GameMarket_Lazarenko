80fc862ad795a8c31ea83dac6f6514f6069469a0
SofiaLazarenko <23a_lsa@liceum.ztu.edu.ua>
2024-05-12 22:48:58 +0300

    п

--- n/market.js
+++ n/market.js
@@ -1,10 +1,22 @@
-console.log('Перевірка')
+let itemsArray=['Газонокосарка 43',
+'Електричний тример 110',
+'Електрична газонокосарка 32',
+'Акумуляторний оприскувач 12 N',
+// 'Газонокосарка 430',
+// 'Електричний тример 130',
+// 'Елекртична газонокосарка 320',
+// 'Акамуляторний оприскувач 12 E'
+]
 
-let itemDiv = document.getElementById("items");
 
-console.log(itemDiv)
 
-if(itemDiv){
+//console.log('Перевірка')
+
+let itemsDiv = document.getElementById("items");
+
+//console.log(itemDiv)
+
+if(itemsDiv){
 
     //  console.log(itemDiv)
     // // console.log("Поле classList:",itemDiv.classList)
@@ -26,6 +38,14 @@ if(itemDiv){
     // for(let i=0; i<100; i++){
     //     itemDiv.innerHTML+='<div class="item"></div>'
     // }
+itemsArray.forEach((item,index) =>{
+    itemsDiv.innerHTML += 
+    `<div class ="item">
+        <h2>Товар №${index+1} з 4</h2>
+         <p>${item}</p>
+    </div>
+    `
+})
 
 
 
@@ -35,12 +55,10 @@ if(itemDiv){
 
 
 
-let itemsArray=['r',
-'r',
-'r',
-'r']
 
-itemsArray=itemsArray.sort()
-for(let i=0; i<itemsArray.length[1];i++){
-    console.log(i+ 'й елеиент',itemsArray[i])
-}
+// itemsArray=itemsArray.sort()
+// for(let i=0; i<itemsArray.length;i++){
+//     console.log(i+ '-й елемент',itemsArray[i])
+// }
+
+
